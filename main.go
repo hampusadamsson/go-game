38e353f980d597689dfd92fe5a7ce4012a524cbf
Your Name <you@example.com>
2022-05-30 23:28:52 +0200

    Fixed game layout

--- main.go
+++ main.go
@@ -1,24 +1,6 @@
-// Copyright 2018 The Ebiten Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-//go:build example
-// +build example
-
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"image"
 	_ "image/png"
@@ -27,17 +9,16 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
+	"github.com/hampusadamsson/go-game/game"
 )
 
 const (
-	screenWidth  = 240
-	screenHeight = 240
+	screenWidth  = 120
+	screenHeight = 120
 )
 
 const (
-	tileSize = 16
-	tileXNum = 25
+	tileSize = 15
 )
 
 var (
@@ -45,11 +26,7 @@ var (
 )
 
 func init() {
-	// Decode an image from the image file's byte slice.
-	// Now the byte slice is generated with //go:generate for Go 1.15 or older.
-	// If you use Go 1.16 or newer, it is strongly recommended to use //go:embed to embed the image file.
-	// See https://pkg.go.dev/embed for more details.
-	img, _, err := image.Decode(bytes.NewReader(images.Tiles_png))
+	img, _, err := ebitenutil.NewImageFromFile("props/advancewars.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,7 +34,8 @@ func init() {
 }
 
 type Game struct {
-	layers [][]int
+	game    [][]game.Tile
+	message interface{}
 }
 
 func (g *Game) Update() error {
@@ -65,23 +43,24 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Draw each tile with each DrawImage call.
-	// As the source images of all DrawImage calls are always same,
-	// this rendering is done very efficiently.
-	// For more detail, see https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#Image.DrawImage
-	const xNum = screenWidth / tileSize
-	for _, l := range g.layers {
-		for i, t := range l {
+	for i, l := range g.game {
+		for j, tile := range l {
 			op := &ebiten.DrawImageOptions{}
-			op.GeoM.Translate(float64((i%xNum)*tileSize), float64((i/xNum)*tileSize))
-
-			sx := (t % tileXNum) * tileSize
-			sy := (t / tileXNum) * tileSize
-			screen.DrawImage(tilesImage.SubImage(image.Rect(sx, sy, sx+tileSize, sy+tileSize)).(*ebiten.Image), op)
+			// Where to draw it
+			op.GeoM.Translate(float64(j*tileSize), float64(i*tileSize))
+			// What to draw
+			x, y, size, _ := tile.Img.GetImage()
+			screen.DrawImage(tilesImage.SubImage(image.Rect(x, y, x+size, y+size)).(*ebiten.Image), op) // What part of a larger image to draw
+			if u, _ := tile.GetUnit(); u != nil {
+				x, y, size, animation := u.Img.GetImage()
+				if animation {
+					op.GeoM.Scale(1, 0.98)
+				}
+				screen.DrawImage(tilesImage.SubImage(image.Rect(x, y, x+size, y+size)).(*ebiten.Image), op)
+			}
 		}
 	}
-
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
+	ebitenutil.DebugPrintAt(screen, fmt.Sprintf("%+v\n", g.message), 12, 12)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -90,68 +69,30 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	g := &Game{
-		layers: [][]int{
-			{
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 218, 243, 243, 243, 243, 243, 243, 243, 243, 243, 218, 243, 244, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 244, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 219, 243, 243, 243, 219, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-				243, 218, 243, 243, 243, 243, 243, 243, 243, 243, 243, 244, 243, 243, 243,
-				243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243, 243,
-			},
-			{
-				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 26, 27, 28, 29, 30, 31, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 51, 52, 53, 54, 55, 56, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 76, 77, 78, 79, 80, 81, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 101, 102, 103, 104, 105, 106, 0, 0, 0, 0,
-
-				0, 0, 0, 0, 0, 126, 127, 128, 129, 130, 131, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 303, 303, 245, 242, 303, 303, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 245, 242, 0, 0, 0, 0, 0, 0,
-			},
-		},
+		message: "",
+		game:    game.GetExampleMap(),
 	}
 
 	ebiten.SetWindowSize(screenWidth*3, screenHeight*3)
-	ebiten.SetWindowTitle("Tiles (Ebiten Demo)")
-	fmt.Println("TEST")
-	go warp(&g.layers)
+	ebiten.SetWindowTitle("Go-Game")
+	go g.warp()
+
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func warp(field *[][]int) {
-	var cur = *field
+// warp adds a 'bouncy' animation to units
+func (g *Game) warp() {
+	var cur = g.game
 	for {
 		time.Sleep(time.Millisecond * 1000)
 		for i := 0; i < len(cur); i++ {
-
-			var t = cur[i]
-			for j := 0; j < len(t)-1; j++ {
-				var jj = (j + 1) % (len(t) - 1)
-				var val = cur[i][jj]
-				cur[i][j] = val
+			for j := 0; j < len(cur[i]); j++ {
+				var tile = &cur[i][j]
+				if unit, err := tile.GetUnit(); err == nil {
+					unit.Img.ToggleAnimation()
+				}
 			}
 		}
 	}
